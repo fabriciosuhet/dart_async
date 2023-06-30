139a7e6d7d51b8394bb73fea90a1cfdfa1f8b555
Fabrício Suhet <fabriciosuhet@hotmail.com>
2023-06-30 15:17:31 -0300

    take, serve para definir quantas vezes a funcao stream vai ser chamada

---
+++ lib/10streams/take2.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: avoid_print
+
+Future<void> main() async {
+  print('INICIO');
+  const interval = Duration(seconds: 2);
+  var stream = Stream<int>.periodic(interval, callback);
+
+  stream = stream.take(5);
+
+// Await for
+  await for (var i in stream) {
+    print(i);
+  }
+  print('FIM');
+}
+
+int callback(int value) {
+  print('O valor é $value');
+  return (value + 1) * 2;
+}
